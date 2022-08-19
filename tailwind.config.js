e9a92138b8250764ed5f866e77c93b8255a2a8f9
Darius Vollmer <dariusv@outlook.de>
2022-08-19 22:12:15 +0200

    changed tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    ".src/pages/**/*.{js,ts,jsx,tsx}",
-    ".src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
-      ".src/pages/**/*.{js,ts,jsx,tsx}",
-      ".src/components/**/*.{js,ts,jsx,tsx}",
+    "src/pages/**/*.{js,ts,jsx,tsx}",
+    "src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
