21206be8885fa4435da17466d1a43f6dc3e987f6
Darius Vollmer <dariusv@outlook.de>
2022-08-19 22:05:38 +0200

    added TailwindCSS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+      <>
+      <button className={"bg-red-500"}>Hello</button>
+      </>
+  )
+}
+
+export default Home
