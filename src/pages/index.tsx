049563bab3e849455cf54adb0a5c7892061d16af
Darius Vollmer <dariusv@outlook.de>
2022-08-22 15:19:30 +0200

    added Daisy UI to Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
+import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      <button className={"bg-red-500"}>Hello</button>
-      </>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Hello</button>
+      <div className="flex flex-row">
+        <div className="mockup-code justify-center">
+          <pre data-prefix="$">
+            <code>npm i daisyui</code>
+          </pre>
 
-export default Home
+          <pre data-prefix=">" className="text-warning">
+            <code>installing...</code>
+          </pre>
+          <pre data-prefix=">" className="text-success">
+            <code>Done!</code>
+          </pre>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
